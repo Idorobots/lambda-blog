d11b257a947b1d017b3f10d98172191eee12ddd1
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2015-12-25 18:40:58 +0100

    Removed scribblings.

--- info.rkt
+++ info.rkt
@@ -4,8 +4,6 @@
 (define deps '("base"
                "rackunit-lib"
                "scribble-html-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
-(define scribblings '(("scribblings/lambda-blog.scrbl" ())))
 (define pkg-desc "A static blog generator.")
 (define version "0.0")
 (define pkg-authors '(Idorobots))
