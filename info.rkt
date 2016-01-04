9f2d14119a96396312d9445fe0913f09a9090e77
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-04 23:57:22 +0100

    Bye, bye Racket!

--- info.rkt
+++
@@ -1,9 +0,0 @@
-#lang info
-
-(define collection 'multi)
-(define deps '("base"
-               "rackunit-lib"
-               "scribble-html-lib"))
-(define pkg-desc "A static blog generator.")
-(define version "0.0")
-(define pkg-authors '(Idorobots))
