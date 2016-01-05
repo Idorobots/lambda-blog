c460d9d6737b08b467aff24b7dfd1c57ac1c3fa3
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-05 23:49:00 +0100

    Made header & footer JS scripts more configurable.

--- src/lambda_blog/templates/header.clj
+++ src/lambda_blog/templates/header.clj
@@ -1,8 +1,8 @@
 (ns lambda-blog.templates.header
   (:require [lambda-blog.utils :refer [path]]
-            [s-html.tags :refer [head link meta] :as tags]))
+            [s-html.tags :refer [head link meta script] :as tags]))
 
-(defn header [{:keys [path-to-root stylesheets title]}]
+(defn header [{:keys [path-to-root scripts stylesheets title]}]
   (head (meta {:charset :utf-8})
         (tags/title title)
         (map #(link {:rel :stylesheet
@@ -16,6 +16,9 @@
         (link {:rel :icon
                :type "image/png"
                :href (path path-to-root "media/favicon.png")})
+        (map #(script {:type "text/javascript"
+                       :src (path path-to-root %)})
+             scripts)
         (meta {:name :viewport
                :content "width=device-width, initial-scale=1.0"})
         (meta {:name :generator
