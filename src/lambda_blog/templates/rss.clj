d228b7dad11d251e0a4ebb74865dfaa0be889a70
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-05 18:03:27 +0100

    Added RSS feed template.

---
+++ src/lambda_blog/templates/rss.clj
@@ -0,0 +1,34 @@
+(ns lambda-blog.templates.rss
+  (:require [clj-time.core :refer [now]]
+            [lambda-blog.utils :refer [path]]
+            [ring.util.codec :refer [url-encode]]
+            [s-html.tags :refer [deftags link] :as tags]))
+
+(deftags [author category content entry feed id name published updated])
+(def _author author)
+
+(defn rss-feed [{:keys [entries title url]}]
+  ["<?xml version=\"1.0\" encoding=\"utf-8\"?>" ;; FIXME Should be (doctype xml)
+   (feed {:xmlns "http://www.w3.org/2005/Atom"}
+         (tags/title title)
+         (link {:rel :self
+                :href (path url "index.xml")})
+         (link {:href url})
+         (updated (now))
+         (id url)
+         (map (fn [{:keys [author contents filename path-to-root tags timestamp title]}]
+                (entry (tags/title {:type :html}
+                                   title)
+                       (id filename)
+                       (_author (name author)) ;; FIXME Loose the _author.
+                       (updated timestamp)
+                       (published timestamp)
+                       (link {:href (path path-to-root filename)})
+                       (map (fn [t]
+                              (category {:scheme (path url (format "/tags/%s.html" (url-encode t)))
+                                         :term t
+                                         :label t}))
+                            tags)
+                       (content {:type :html}
+                                contents)))
+              entries))])
