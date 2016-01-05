a09b741a2307920d2c92b19188533ca0d4af9993
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-05 21:38:14 +0100

    Added recent articles template.

---
+++ src/lambda_blog/templates/recent.clj
@@ -0,0 +1,51 @@
+(ns lambda-blog.templates.recent
+  (:require [clj-time.format :refer [formatter parse unparse]]
+            [lambda-blog.templates.banner :refer [banner]]
+            [lambda-blog.templates.footer :refer [footer]]
+            [lambda-blog.templates.header :refer [header]]
+            [lambda-blog.templates.nav :refer [navigation]] ;; FIXME REALLY find a way to make this less verbose.
+            [lambda-blog.utils :refer [path]]
+            [ring.util.codec :refer [url-encode]]
+            [s-html.tags :refer [a article body div doctype h1 h4 hr html i nav p span time] :as tags]))
+
+(defn format-date [timestamp]
+  (unparse (formatter "YYYY-MM-dd HH:mm")
+           (parse timestamp)))
+
+(defn entry-summary [{:keys [path-to-root summary tags timestamp title url]}]
+  (article (tags/header
+            (div {:class :well}
+                 (div {:class :row}
+                      (div {:class :text-center}
+                           (h1 (a {:href url}
+                                  title))
+                           (p "Posted on "
+                              (time (format-date timestamp)))
+                           (nav (map (fn [t]
+                                       (span {:class "label label-info small"}
+                                             (a {:class :tag
+                                                 :href (path path-to-root
+                                                             (format "/tags/%s.html" (url-encode t)))}
+                                                t)))
+                                     tags))))))
+           (div {:class :article-content}
+                summary)
+           (tags/footer
+            (hr))))
+
+(defn recent-articles [{:keys [entries] :as ent}]
+  [(doctype :html)
+   (html (header ent)
+         (body (navigation ent)
+               (div {:class :body-wrap}
+                    (article {:id :page}
+                             (div {:class :container}
+                                  (banner ent)
+                                  (map entry-summary entries)
+                                  (div {:class :well}
+                                       (div {:class :row}
+                                            (div {:class :text-center}
+                                                 (h1 (a {:href "./archives.html"}
+                                                   "Archives")))))
+                                  (hr)
+                                  (footer ent))))))])
