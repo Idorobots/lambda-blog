2a14cb752395d75af931b89f16375c71f32d2ca6
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-05 23:25:35 +0100

    Added static post page template.

--- src/lambda_blog/templates/entry.clj
+++ src/lambda_blog/templates/entry.clj
@@ -1,5 +1,6 @@
 (ns lambda-blog.templates.entry
   (:require [clj-time.format :refer [formatter parse unparse]]
+            [lambda-blog.templates.static :refer [static-page-template]]
             [lambda-blog.utils :refer [path]]
             [ring.util.codec :refer [url-encode]]
             [s-html.tags :refer [a article div footer h1 header hr i nav p span time] :as tags]))
@@ -8,26 +9,35 @@
   (unparse (formatter "YYYY-MM-dd HH:mm")
            (parse timestamp)))
 
-(defn entry-summary [{:keys [path-to-root summary tags timestamp title url]}]
-  (article (header
-            (div {:class :well}
-                 (div {:class :row}
-                      (div {:class :text-center}
-                           (h1 (a {:href url}
-                                  title))
-                           (p "Posted on "
-                              (time (format-date timestamp)))
-                           (nav (map (fn [t]
-                                       (span {:class "label label-info small"}
-                                             (a {:class :tag
-                                                 :href (path path-to-root
-                                                             (format "/tags/%s.html" (url-encode t)))}
-                                                t)))
-                                     (sort tags)))))))
-           (div {:class :article-content}
-                summary
-                (p (a {:href url}
-                      "Continue reading "
-                      (i {:class "fa fa-arrow-right"}))))
-           (footer
-            (hr))))
+(defn entry-template [contents-template {:keys [path-to-root tags timestamp title url] :as ent}]
+  (article
+   (header
+    (div {:class :well}
+         (div {:class :row}
+              (div {:class :text-center}
+                   (h1 (a {:href url}
+                          title))
+                   (p "Posted on "
+                      (time (format-date timestamp)))
+                   (nav (map (fn [t]
+                               (span {:class "label label-info small"}
+                                     (a {:class :tag
+                                         :href (path path-to-root
+                                                     (format "/tags/%s.html" (url-encode t)))}
+                                        t)))
+                             (sort tags)))))))
+   (div {:class :article-content}
+        (contents-template ent))
+   (footer
+    (hr))))
+
+(def entry-summary (partial entry-template
+                            (fn [{:keys [summary url]}]
+                              [summary
+                               (p (a {:href url}
+                                     "Continue reading "
+                                     (i {:class "fa fa-arrow-right"})))])))
+
+(def entry (partial entry-template :contents)) ;; TODO Add prev/next post.
+
+(def entry-page (partial static-page-template entry))
