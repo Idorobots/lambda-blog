b0580c09a9569eb990e308b7d0edd31d00197197
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-07 01:10:05 +0100

    Refactored recent articles template to use static-page-template.

--- src/lambda_blog/templates/recent.clj
+++ src/lambda_blog/templates/recent.clj
@@ -1,36 +1,25 @@
 (ns lambda-blog.templates.recent
-  (:require [lambda-blog.templates.banner :refer [banner]]
-            [lambda-blog.templates.entry :refer [entry-summary]]
-            [lambda-blog.templates.footer :refer [footer]]
-            [lambda-blog.templates.header :refer [header]]
-            [lambda-blog.templates.nav :refer [navigation]] ;; FIXME REALLY find a way to make this less verbose.
+  (:require [lambda-blog.templates.entry :refer [entry-summary]]
+            [lambda-blog.templates.static :refer [static-page-template]]
             [lambda-blog.utils :refer [path]]
-            [s-html.tags :refer [a article body div doctype h1 hr html]]))
+            [s-html.tags :refer [a div h1 hr]]))
 
-(defn filtered-articles [entry-filter {:keys [entries] :as ent}]
-  [(doctype :html)
-   (html (header ent)
-         (body (navigation ent)
-               (div {:class :body-wrap}
-                    (article {:id :page}
-                             (div {:class :container}
-                                  (banner ent)
-                                  (map entry-summary
-                                       (entry-filter entries))
-                                  (div {:class :well}
-                                       (div {:class :row}
-                                            (div {:class :text-center}
-                                                 (h1 (a {:href "./archives.html"}
-                                                        "Archives")))))
-                                  (hr)
-                                  (footer ent))))))])
+(defn filtered-articles [entry-filter]
+  (partial static-page-template
+           (fn [{:keys [entries]}]
+             [(map entry-summary
+                   (entry-filter entries))
+              (div {:class :well}
+                   (div {:class :row}
+                        (div {:class :text-center}
+                             (h1 (a {:href "./archives.html"}
+                                    "Archives")))))
+              (hr)])))
 
-(def recent-articles
-  (partial filtered-articles
-           #(take 15 %)))
+(def recent-articles (filtered-articles #(take 15 %)))
 
 (defn articles-by-tag [tag entity]
-  (filtered-articles #(filter (fn [{:keys [tags]}]
+  ((filtered-articles #(filter (fn [{:keys [tags]}]
                                 (contains? tags tag))
-                              %)
-                     entity))
+                               %))
+   entity))
