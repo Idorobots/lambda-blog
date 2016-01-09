883c09219e58e6c8deedc0a23d459784007c9b5f
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-09 22:40:31 +0100

    Moved lambda-blog attribution to a separate template.

--- src/lambda_blog/templates/footer.clj
+++ src/lambda_blog/templates/footer.clj
@@ -3,14 +3,17 @@
             [lambda-blog.utils :refer [pathcat]]
             [s-html.tags :refer [a div hr p script] :as tags]))
 
+(defn powered-by []
+  (p "Powered by "
+     (a {:href "https://github.com/Idorobots/lambda-blog"}
+        "λ-blog")
+     "."))
+
 (defn footer [{:keys [footer-contents footer-scripts path-to-root]}]
   [(tags/footer
     (hr)
     (row
      (text-centered
       footer-contents
-      (p "Powered by "
-         (a {:href "https://github.com/Idorobots/lambda-blog"}
-            "λ-blog")
-         "."))))
+      (powered-by))))
    (map #(javascript (pathcat path-to-root %)) footer-scripts)])
