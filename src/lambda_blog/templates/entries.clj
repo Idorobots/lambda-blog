5dafd29235c5836050ac12b16a3d5adb45cf23e1
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-14 01:09:59 +0100

    Refactored entry templates to include author.

--- src/lambda_blog/templates/entries.clj
+++ src/lambda_blog/templates/entries.clj
@@ -5,17 +5,14 @@
             [lambda-blog.utils :refer [format-date pathcat]]
             [s-html.tags :refer [a article div footer h1 header hr i li nav p span time ul]]))
 
-(defn entry-header [{:keys [path-to-root tags timestamp]} title]
-  (text-centered
-   (h1 title)
-   (p "Posted on " (time (format-date timestamp)))
-   (nav (map #(info-label
-               (a {:class :tag
-                   :href (pathcat path-to-root (:path %))}
-                  (:id %)))
-             (sort-by :id tags)))))
+(defn- entry-tags [{:keys [path-to-root tags]}]
+  (nav (map #(info-label
+              (a {:class :tag
+                  :href (pathcat path-to-root (:path %))}
+                 (:id %)))
+            (sort-by :id tags))))
 
-(defn pager [{:keys [path-to-root]} {:keys [path title] :as link} class]
+(defn- pager [{:keys [path-to-root]} {:keys [path title] :as link} class]
   (div {:class [:col-xs-2 :col-sm-3]}
        (when link
          (nav
@@ -30,27 +27,32 @@
                        (i {:class [:fa :fa-chevron-right]})))))))))
 
 (defn entry
-  [{:keys [contents next previous title] :as ent}]
+  [{:keys [author contents next previous timestamp title] :as ent}]
   (article
    (header
     (well
      (row
       (pager ent previous :previous)
       (div {:class [:col-xs-8 :col-sm-6]}
-           (entry-header ent title))
+           (text-centered (h1 title)
+                          (p "Posted on " (time (format-date timestamp))
+                             " by " author)
+                          (entry-tags ent)))
       (pager ent next :next))))
    contents))
 
 (def entry-page (partial page entry))
 
-(defn entry-summary [{:keys [path path-to-root summary tags title] :as ent}]
+(defn entry-summary [{:keys [path path-to-root summary tags timestamp title] :as ent}]
   (article
-   (-> ent
-       (entry-header (a {:href (pathcat path-to-root path)}
-                        title))
-       row
-       well
-       header)
+   (header
+    (well
+     (row
+      (text-centered
+       (h1 (a {:href (pathcat path-to-root path)}
+              title))
+       (p "Posted on " (time (format-date timestamp)))
+       (entry-tags ent)))))
    summary
    (p (a {:href (pathcat path-to-root path)}
          "Continue reading "
