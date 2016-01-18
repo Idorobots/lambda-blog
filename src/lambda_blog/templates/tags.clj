3f0840c45f87ded51215e5b77a2a6700bbfd20cd
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-19 00:32:02 +0100

    Made tag-cloud template more configurable.

--- src/lambda_blog/templates/tags.clj
+++ src/lambda_blog/templates/tags.clj
@@ -4,10 +4,7 @@
             [lambda-blog.utils :refer [pathcat]]
             [s-html.tags :refer [a div h1 span]]))
 
-(def min-size 100)
-(def max-size 300)
-
-(defn tag-cloud [{:keys [entries path-to-root tags]}]
+(defn tag-cloud [min max {:keys [entries path-to-root tags]}]
   (let [counts (->> tags
                     ;; FIXME O(N_tags * N_entries) but could be O(N_entries)
                     (map #(->> entries
@@ -34,5 +31,5 @@
 (defn tags-index [ent]
   (page (fn [ent]
           [(-> "Tag cloud" h1 text-centered panel)
-           (tag-cloud ent)])
+           (tag-cloud 100 300 ent)])
         ent))
