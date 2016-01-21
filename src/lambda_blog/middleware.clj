4fe39bd87aa2db534c0ee2f28dba2859a947112f
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-21 17:32:44 +0100

    Added docstrings to the lambda-blog.middleware namespace.

--- src/lambda_blog/middleware.clj
+++ src/lambda_blog/middleware.clj
@@ -1,4 +1,5 @@
 (ns lambda-blog.middleware
+  "Various entity transformers used in the generation pipelines."
   (:refer-clojure :exclude [replace])
   (:require [clojure.pprint :refer [pprint]]
             [clojure.set :refer [union]]
@@ -24,21 +25,27 @@
        (times "../")
        (apply str)))
 
-(defn add-paths [path-spec]
+(defn add-paths
+  "Returns a middleware function that adds paths to an `entity` based on `path-spec`. `path-spec` can use various `entity` keys by naming them in angle brackets (i.e. `\"posts/<year>/<month>/<title>.html\"`). Each key is stringified and [[lambda-blog.utils/sanitize]]d before including in the path."
+  [path-spec]
   (fn [entity]
     (let [p (fmt path-spec entity)]
       (assoc entity
              :path-to-root (path-to-root path-spec)
              :path (pathcat p)))))
 
-(defn collect-tags [{:keys [entries] :as ent}]
+(defn collect-tags
+  "Collects unique `tags` from each of the `entries` in the `ent`ity. Returns `ent`ity with collected tags stored under `tags`."
+  [{:keys [entries] :as ent}]
   (->> entries
        (map :tags)
        (apply union)
        (into #{})
        (assoc ent :tags)))
 
-(defn link [es]
+(defn link
+  "Links all entities in `es` by adding `:previous` and `:next` keys containing respective entities to each of them."
+  [es]
   (map (fn [prev curr next]
          (assoc curr
                 :previous prev
@@ -47,13 +54,17 @@
        es
        (concat (next es) '(nil))))
 
-(defn promote [what]
+(defn promote
+  "Returns a middleware function that promotes all subkeys of a map under `what` key of an `entity` into the `entity`."
+  [what]
   (fn [entity]
     (reduce (fn [e [k v]]
               (assoc e k v))
             entity
             (entity what))))
 
-(defn inspect [entity]
+(defn inspect
+  "Pretty prints an `entity` for debugging purposes. Returns unmodified `entity`."
+  [entity]
   (log/debug (with-out-str (pprint entity)))
   entity)
