f18072ba0907ab08abafb5e39540fadd0df29053
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-22 20:05:38 +0100

    Added docstrings to the lambda-blog.templates.tags namespace.

--- src/lambda_blog/templates/tags.clj
+++ src/lambda_blog/templates/tags.clj
@@ -4,7 +4,9 @@
             [lambda-blog.utils :refer [pathcat]]
             [s-html.tags :refer [a div h1 span]]))
 
-(defn tag-cloud [min max {:keys [entries path-to-root tags]}]
+(defn tag-cloud
+  "Creates a tag cloud of `tags` where each element is scaled according to the number of `entries` that are tagged by it. `min` and `max` specify in percent the minimal and maximal font size of the elements."
+  [min max {:keys [entries path-to-root tags]}]
   (let [counts (->> tags
                     ;; FIXME O(N_tags * N_entries) but could be O(N_entries)
                     (map #(->> entries
@@ -28,7 +30,9 @@
                (sort-by (comp :id first)
                         counts))))))
 
-(defn tags-index [ent]
+(defn tags-index
+  "Creates an HTML page containing a [[tag-cloud]]."
+  [ent]
   (page (fn [ent]
           [(-> "Tag cloud" h1 text-centered panel)
            (tag-cloud 100 300 ent)])
