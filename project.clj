806780d0433101f16c34f2e599ce619be68cea97
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-01-30 18:07:56 +0100

    Oooh yeah.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lambda-blog "1.0.0-SNAPSHOT"
+(defproject lambda-blog "1.0.0"
   :description "A static blog generator."
   :url "https://github.com/Idorobots/lambda-blog"
   :license {:name "MIT License"
