b1edebe0e1410910296cf04990e4600fca1a0154
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-02-22 04:05:38 +0100

    Bumped lambda-blog patch version.

--- lein-lambda-blog/src/leiningen/new/lambda_blog/project.clj
+++ lein-lambda-blog/src/leiningen/new/lambda_blog/project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[lambda-blog/lambda-blog "1.1.0"]
+  :dependencies [[lambda-blog/lambda-blog "1.1.1"]
                  [org.clojure/clojure "1.7.0"]
                  [s-html "0.1.6"]]
   :main {{name}}.core)
