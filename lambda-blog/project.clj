0214cd1ad2509d41bcde797cec6b89fac55c13e3
Kajetan Rzepecki <kajetan.rzepecki+github@gmail.com>
2016-02-22 03:54:21 +0100

    Bumped minor version, because features!

--- lambda-blog/project.clj
+++ lambda-blog/project.clj
@@ -1,4 +1,4 @@
-(defproject lambda-blog/lambda-blog "1.0.7"
+(defproject lambda-blog/lambda-blog "1.1.0"
   :description "A static blog generator."
   :url "https://github.com/Idorobots/lambda-blog"
   :license {:name "MIT License"
